c0d0d270b17a1de5bf3ca70ffdb8764fb1b65743
NateMIC <nathan.willeme@mic-belgique.be>
2022-03-01 16:19:49 +0100

    Add @microsoft/signalR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignalRService } from "./service/signal-r.service";
 import { PrimeNGConfig } from 'primeng/api';
+import { HttpClient } from '@angular/common/http';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
     menuMode = 'static';
 
-    constructor(private primengConfig: PrimeNGConfig) { }
+    constructor(private primengConfig: PrimeNGConfig, public signalRService: SignalRService, private http: HttpClient) { }
 
     ngOnInit() {
         this.primengConfig.ripple = true;
         document.documentElement.style.fontSize = '14px';
+        this.signalRService.startConnection();
+        this.signalRService.addTransferDataListener();
+        this.startHttpRequest();
+    }
+
+    private startHttpRequest(){
+        this.http.get("https://localhost:5001/api/chart").subscribe(res => {
+            console.log(res);
+        })
     }
 }
