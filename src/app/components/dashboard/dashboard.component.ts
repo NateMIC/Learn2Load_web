017c4275e20e32db0b74718636b5c6d785d23e66
MaximeMIC <maxime.demaret@mic-belgique.be>
2022-03-01 16:04:05 +0100

    Adapted form's values to modes

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
-import { Product } from '../../api/product';
 import { ProductService } from '../../service/productservice';
 import { Subscription } from 'rxjs';
 import { ConfigService } from '../../service/app.config.service';
@@ -20,15 +18,15 @@ export class DashboardComponent implements OnInit {
 
     config: AppConfig;
 
-    succesLevel: number = 50;
+    succesLevel: number;
 
-    errorLevel: number = 15;
+    errorLevel: number;
 
-    crateNumber: number = 6;
+    crateNumber: number;
 
-    minutes : number = 3;
+    minutes : number;
 
-    secondes : number = 30;
+    secondes : number;
 
     learninglevel: any[];
 
@@ -39,16 +37,81 @@ export class DashboardComponent implements OnInit {
         this.learninglevel = [
             {label: 'Débutant', value: 'DE'},
             {label: 'Intermédiaire', value: 'IN'},
+            {label: 'Avancé', value: 'AV'},
             {label: 'Expert', value: 'EX'},
             {label: 'Personnalisé', value: 'PE'}
         ];
     }
+
+
 
     ngOnInit() {
         this.config = this.configService.config;
         this.subscription = this.configService.configUpdate$.subscribe(config => {
             this.config = config;
         });
+
+        this.succesLevel = 50;
+        this.errorLevel = 15;
+        this.crateNumber = 4;
+        this.minutes = 15;
+        this.secondes = 0;
+    }
+
+    changeValuesBasedOnTheSelectedLevel(value:string) {
+        switch(value) {
+            case "DE" : console.log("'Débutant' mode activated");
+                this.succesLevel = 40;
+                this.errorLevel = 35;
+                this.crateNumber = 4;
+                this.minutes = 15;
+                this.secondes = 0;
+            break;
+            case "IN" : console.log("'Intermédiaire' mode activated");
+                this.succesLevel = 50;
+                this.errorLevel = 25;
+                this.crateNumber = 6;
+                this.minutes = 10;
+                this.secondes = 0;
+            break;
+            case "AV" : console.log("'Avancé' mode activated");
+                this.succesLevel = 60;
+                this.errorLevel = 15;
+                this.crateNumber = 8;
+                this.minutes = 7;
+                this.secondes = 30;
+            break;
+            case "EX" : console.log("'Expert' mode activated");
+                this.succesLevel = 70;
+                this.errorLevel = 10;
+                this.crateNumber = 10;
+                this.minutes = 5;
+                this.secondes = 0;
+            break;
+            case "PE" : console.log("'Personnalisé' mode activated");
+                console.log("In progress");
+            break;
+        }      
+    }
+
+    changeToPersonnalizedMode() {
+        if(this.selectedLevel != "PE") {
+            this.selectedLevel = "PE";
+            console.log("'Personnalisé' mode activated");
+        }
+    }
+
+    savePersonnalizedValues() {
+        this.succesLevel = this.succesLevel;
+        this.errorLevel = this.errorLevel;
+        this.crateNumber = this.crateNumber;
+        this.minutes = this.minutes;
+        this.secondes = this.secondes;
+
+        // console.log("Success : " + this.succesLevel);
+        // console.log("Error : " + this.errorLevel);
+        // console.log("Nb crates : " + this.crateNumber);
+        // console.log("Time : " + this.minutes + ":" + this.secondes);
     }
 
 }
