34ebb50192fd95261bdf76aa68a668de3d86cea3
NateMIC <nathan.willeme@mic-belgique.be>
2022-05-13 15:47:57 +0200

    edit server url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlServer: "https://learn2loadsignalrserver.azurewebsites.net/holo"
+  urlServer: "https://learn2loadserversignalr.azurewebsites.net/holo"
 };
